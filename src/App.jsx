9351dbc8e618f43286b8a1e067cbc018b8e3be2e
Nihat Algan <burakalgan@gmail.com>
2024-02-08 22:15:04 +0100

    done iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
 import "./App.css";
 
 function App() {
+  const [foods, setProducts] = useState(foodsJson);
+  // console.log(foods[0].name);
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      {/* <FoodBox /> */}
+      <FoodBox
+        food={{
+          name: "Orange",
+          calories: 85,
+          image: "https://i.imgur.com/abKGOcv.jpg",
+          servings: 1,
+        }}
+      />
+
+      {/* {foods.map((food) => {
+        return <p>{food.name}</p>;
+      })} */}
     </div>
   );
 }
