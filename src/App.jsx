0d462a779b59458c027ca5463957ff939dc1844a
Nihat Algan <burakalgan@gmail.com>
2024-02-09 22:06:44 +0100

    done without style

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,10 @@
-import { useState } from "react";
-import foodsJson from "./foods.json";
-import FoodBox from "./components/FoodBox";
-import AddFood from "./components/AddFoodForm";
+import FoodList from "./components/FoodList";
 import "./App.css";
 
 function App() {
-  const [foods, setProducts] = useState(foodsJson);
-  const [foodsData, setProductsData] = useState(foodsJson);
-
-  const deleteFood = (foodId) => {
-    const filteredFoods = foods.filter((food) => {
-      return food.id !== foodId;
-    });
-
-    setProducts(filteredFoods);
-  };
-
-  const addNewFood = (food) => {
-    const updatedFoods = [...foods, food];
-    const updatedFoodsData = [...foodsData, food];
-
-    setProducts(updatedFoods); // update the state variable "movies" with updatedMovies
-    setProductsData(updatedFoodsData);
-  };
-
   return (
     <div className="App">
-      <h1>LAB | React IronNutrition</h1>
-      <AddFood addFood={addNewFood} />
-
-      {/* <FoodBox
-        food={{
-          name: "Orange",
-          calories: 85,
-          image: "https://i.imgur.com/abKGOcv.jpg",
-          servings: 1,
-        }}
-      /> */}
-
-      {foods.map((food) => {
-        return <FoodBox key={food.id} food={food} deleteFood={deleteFood} />;
-      })}
+      <FoodList />
     </div>
   );
 }
---
+++ src/components/FoodList.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import foodsJson from "../foods.json";
+import FoodBox from "./FoodBox";
+import AddFood from "./AddFoodForm";
+
+function FoodList() {
+  const [foods, setProducts] = useState(foodsJson);
+  const [foodsData, setProductsData] = useState(foodsJson);
+
+  const deleteFood = (foodId) => {
+    const filteredFoods = foods.filter((food) => {
+      return food.id !== foodId;
+    });
+
+    setProducts(filteredFoods);
+  };
+
+  const addNewFood = (food) => {
+    const updatedFoods = [...foods, food];
+    const updatedFoodsData = [...foodsData, food];
+
+    setProducts(updatedFoods); // update the state variable "movies" with updatedMovies
+    setProductsData(updatedFoodsData);
+  };
+
+  return (
+    <div className="App">
+      <h1>LAB | React IronNutrition</h1>
+      <AddFood addFood={addNewFood} />
+
+      {/* <FoodBox
+          food={{
+            name: "Orange",
+            calories: 85,
+            image: "https://i.imgur.com/abKGOcv.jpg",
+            servings: 1,
+          }}
+        /> */}
+
+      {foods.map((food) => {
+        return <FoodBox key={food.id} food={food} deleteFood={deleteFood} />;
+      })}
+    </div>
+  );
+}
+
+export default FoodList;
