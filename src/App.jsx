160dd75286998f6e46e4e292c90486be0a4173cb
Nihat Algan <burakalgan@gmail.com>
2024-02-09 21:59:34 +0100

    done iteration 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import foodsJson from "./foods.json";
 import FoodBox from "./components/FoodBox";
+import AddFood from "./components/AddFoodForm";
 import "./App.css";
 
 function App() {
   const [foods, setProducts] = useState(foodsJson);
+  const [foodsData, setProductsData] = useState(foodsJson);
 
   const deleteFood = (foodId) => {
     const filteredFoods = foods.filter((food) => {
@@ -14,9 +16,18 @@ function App() {
     setProducts(filteredFoods);
   };
 
+  const addNewFood = (food) => {
+    const updatedFoods = [...foods, food];
+    const updatedFoodsData = [...foodsData, food];
+
+    setProducts(updatedFoods); // update the state variable "movies" with updatedMovies
+    setProductsData(updatedFoodsData);
+  };
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <AddFood addFood={addNewFood} />
 
       {/* <FoodBox
         food={{
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,83 @@
 // Your code here
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
+
+function AddFood({ addFood }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [calories, setCalories] = useState(0);
+  const [servings, setServings] = useState(0);
+
+  const handleNameInput = (e) => setName(e.target.value);
+  const handleImageInput = (e) => setImage(e.target.value);
+  const handleCaloriesInput = (e) => setCalories(e.target.value);
+  const handleServingsInput = (e) => setServings(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevents the form from reloading the page
+    // Add the logic to create a new movie and add it to the state variable movies
+    const id = uuidv4();
+    const newFood = {
+      id,
+      name,
+      image,
+      calories,
+      servings,
+    };
+    console.log("newFood", newFood);
+    addFood(newFood); // updates the foods state variable
+    // this resets the form to its initial state
+    setName("");
+    setImage("");
+    setCalories(0);
+    setServings(0);
+  };
+
+  return (
+    <div className="add_food">
+      <h4>Add a Food</h4>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Food Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleNameInput}
+          />
+        </div>
+        <div>
+          <label>Image link:</label>
+          <input
+            type="text"
+            name="image"
+            value={image}
+            onChange={handleImageInput}
+          />
+        </div>
+        <div>
+          <label>Calories:</label>
+          <input
+            type="number"
+            name="calories"
+            value={calories}
+            onChange={handleCaloriesInput}
+          />
+        </div>
+        <div>
+          <label>Servings:</label>
+          <input
+            type="number"
+            name="servings"
+            value={servings}
+            onChange={handleServingsInput}
+          />
+        </div>
+
+        <button type="submit">Add a Food</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddFood;
